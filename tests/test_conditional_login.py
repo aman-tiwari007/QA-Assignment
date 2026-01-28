fa16b17686eb40182892e6c1f820920cb3d42a70
aman-tiwari007 <eramantiwarije007@gmail.com>
2026-01-28 22:40:55 +0530

    Update test_conditional_login.py

--- tests/test_conditional_login.py
+++ tests/test_conditional_login.py
@@ -1,1 +1,15 @@
+from playwright.sync_api import expect
+
+def test_conditional_login(page):
+    page.click("text=Flaky Selectors")
+
+    page.click("text=Admin User")
+    expect(page.get_by_text("Admin Panel")).to_be_visible()
+    expect(page.get_by_text("Standard Panel")).not_to_be_visible()
+
+    page.click("text=Logout")
+
+    page.click("text=Standard User")
+    expect(page.get_by_text("Standard Panel")).to_be_visible()
+    expect(page.get_by_text("Admin Panel")).not_to_be_visible()
 
