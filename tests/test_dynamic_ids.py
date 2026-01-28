a6cfc5e9b76d25d0d3fd597b7a897d6ab753446c
aman-tiwari007 <eramantiwarije007@gmail.com>
2026-01-28 23:40:19 +0530

    Update test_dynamic_ids.py

--- tests/test_dynamic_ids.py
+++ tests/test_dynamic_ids.py
@@ -1,11 +1,6 @@
-from playwright.sync_api import expect
-
 def test_dynamic_ids(page):
-    page.click("text=Flaky Selectors")
-    page.click("text=Regenerate All IDs")
+    frame = page.frame_locator("iframe")
 
-    beta_item = page.get_by_text("Beta")
-    beta_item.click()
-
-    expect(beta_item).to_have_class("selected")
+    frame.get_by_text("Flaky Selectors").click()
+    frame.locator("[id^='dynamic']").first.click()
 
